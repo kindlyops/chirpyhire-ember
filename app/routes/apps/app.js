960021dca7ecc775a9b5e2a4718d33afb1b83730
Harry Whelchel <harry@chirpyhire.com>
2018-01-28 22:03:18 -0500

    wip left-navigation

--- app/routes/apps/app.js
+++ app/routes/apps/app.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params) {
-    return this.get('store').findRecord('app', params.app_id, { include: 'segments' });
+    return this.get('store').findRecord('app', params.app_id, { 
+      include: 'segments.predicates' 
+    });
   }
 });
